0557c6920b9e108950e11909658b3ba716a6976c
mahoo <146084234+ADM-olivier@users.noreply.github.com>
2025-03-24 21:37:44 +0400

    initial commit/ we finished writing first javascript homework

---
+++ script.js
@@ -0,0 +1,42 @@
+// 1.1 დავალება
+{
+    var counter = 5 + 1;
+    window.alert(counter);
+
+}
+
+// 1.2 დავალება
+var num = 5;
+num = 10;
+window.alert(num);
+ 
+
+// 1.3 დავალება
+{
+    let b = 10+20+30;
+    window.alert(b);
+}    
+
+// 1.4 დავალება
+var a = 100 ;
+var b = 40;
+var c = a - b;
+var d = 20;
+console.log(c + d);
+
+// 1.5 დავალება
+console.log(13)
+
+// 1.6 დავლება
+{
+    let a = 13;
+    let b =5;
+    console.log(a % b)
+}
+    
+// 1.7 დავალება
+    console.log(72);
+
+// 1.8 დავალება
+    var birthDay ='11.06.2003';
+    console.log(birthDay)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+// 1.1 დავალება
+// {
+//     var counter = 5 + 1;
+//     window.alert(counter);
+
+// }
+
+// // 1.2 დავალება
+// var num = 5;
+// num = 10;
+// window.alert(num);
+
+
+// // 1.3 დავალება
+// {
+//     let b = 10+20+30;
+//     window.alert(b);
+// }    
